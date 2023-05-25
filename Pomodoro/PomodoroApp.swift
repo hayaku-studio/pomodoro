bed3f362044982f4b26b8ff5a286435ddae3934e
David Speers <speerscd@gmail.com>
2023-05-25 16:25:36 +0100

    change app to menu bar app
    Tutorial:
    https://sarunw.com/posts/swiftui-menu-bar-app/

--- Pomodoro/PomodoroApp.swift
+++ Pomodoro/PomodoroApp.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 @main
 struct PomodoroApp: App {
+    @State var currentNumber: String = "1"
+        
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
+            Button("One") {
+                currentNumber = "1"
+            }
+            .keyboardShortcut("1")
+            Button("Two") {
+                currentNumber = "2"
+            }
+            .keyboardShortcut("2")
+            Button("Three") {
+                currentNumber = "3"
+            }
+            .keyboardShortcut("3")
+            Divider()
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }.keyboardShortcut("q")
         }
     }
 }
