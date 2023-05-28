4b1b4d6288f33a12d96a85454fb2dedcc27274ad
David Speers <speerscd@gmail.com>
2023-05-28 12:45:34 +0100

    refactor code

--- Pomodoro/PomodoroApp.swift
+++ Pomodoro/PomodoroApp.swift
@@ -20,7 +20,7 @@ struct PomodoroApp: App {
 
 class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
     private var statusItem: NSStatusItem!
-    private var popover: NSPopover!
+    private let popover = NSPopover()
     
     @MainActor func applicationDidFinishLaunching(_ notification: Notification) {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
@@ -30,16 +30,14 @@ class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
             statusButton.action = #selector(togglePopover)
         }
         
-        self.popover = NSPopover()
-        self.popover.contentSize = NSSize(width: 300, height: 300)
-        self.popover.behavior = .transient
-        self.popover.contentViewController = NSHostingController(rootView: AnimationView())
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: AnimationView())
     }
     
     @objc func togglePopover() {
         if let button = statusItem.button {
             if popover.isShown {
-                self.popover.performClose(nil)
+                popover.performClose(nil)
             } else {
                 popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
                 popover.contentViewController?.view.window?.makeKey()
