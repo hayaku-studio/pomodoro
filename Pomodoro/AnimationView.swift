e3a62decc224f85b1fbf88520c975008c917b379
David Speers <speerscd@gmail.com>
2023-05-28 19:09:44 +0100

    add FontIcon.button and ensure it isn't "tab-selected" by default by having a hidden Button

--- Pomodoro/AnimationView.swift
+++ Pomodoro/AnimationView.swift
@@ -6,15 +6,26 @@
 //
 
 import SwiftUI
+import SwiftUIFontIcon
 import RiveRuntime
 
 struct AnimationView: View {
+    let pomodoro = RiveViewModel(fileName: "pomodoro_timer", stateMachineName: "State Machine")
+    
     var body: some View {
         VStack {
-            Text("Hello")
-            RiveViewModel(fileName: "pomodoro_timer").view().scaledToFit().frame(width: 200, height: 200)
-            Text("There")
+            Button(action: {}) {
+                Text("")
+            }
+            .opacity(0)
+            .frame(height: 0)
+            pomodoro.view().scaledToFit().frame(width: 200, height: 200)
+            FontIcon.button(.materialIcon(code: .play_circle_filled), action: startTimer)
         }
+    }
+    
+    func startTimer() {
+        print("Hello")
     }
 }
 
