f55977abe103eac88c2828736bd238076e44d811
David Speers <speerscd@gmail.com>
2023-06-05 17:44:26 +0100

    change menubar to be two pomodoro timers

--- Pomodoro/Views/PomodoroIconView.swift
+++ Pomodoro/Views/PomodoroIconView.swift
@@ -11,14 +11,24 @@ struct PomodoroIconView: View {
     @EnvironmentObject private var modelData: ModelData
 
     var body: some View {
-        ZStack {
-            Image("Tomato Icon Wide")
-            Text("\(String(format: "%02d", Int(modelData.pomoTimer.timeSeconds/60)))" +
-                 ":" +
-                 "\(String(format: "%02d", Int(modelData.pomoTimer.timeSeconds % 60)))")
-            .font(.footnote)
-            .offset(y: 2)
-            .foregroundColor(Color("Dark Mode Contrast"))
+        HStack {
+            ZStack {
+                Image("Tomato Icon Semi Wide")
+                Text("\(String(format: "%02d", Int(modelData.pomoTimer.timeSeconds/60)))")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .offset(y: 2)
+                    .foregroundColor(Color("Dark Mode Contrast"))
+            }.offset(x: 6)
+            Text(":").offset(x: -2)
+             ZStack {
+                    Image("Tomato Icon Semi Wide")
+                    Text("\(String(format: "%02d", Int(modelData.pomoTimer.timeSeconds%60)))")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .offset(y: 2)
+                .foregroundColor(Color("Dark Mode Contrast"))
+             }.offset(x: -6)
         }
     }
     
