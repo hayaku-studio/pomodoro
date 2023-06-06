37cfdb641d13f1f36b2b24e2ba8f958d8bb60350
David Speers <speerscd@gmail.com>
2023-06-06 14:22:54 +0100

    decrease relative volume
    https://stackoverflow.com/questions/56813508/how-to-set-volume-for-avplayer

--- Pomodoro/Utils/Sound.swift
+++ Pomodoro/Utils/Sound.swift
@@ -13,6 +13,7 @@ func playSound() {
     guard let url = Bundle.main.url(forResource: "Reception Bell", withExtension: "mp3") else { return }
     do {
         player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+        player?.volume = 0.1 // relative to system audio
         guard let player = player else { return }
         player.play()
     } catch let error {
