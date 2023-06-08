cd0855ead780163a714d0871810318e5daaa9c1d
David Speers <speerscd@gmail.com>
2023-06-08 10:35:07 +0100

    add background that dismisses SettingsView modal

--- Pomodoro/Views/PopupView.swift
+++ Pomodoro/Views/PopupView.swift
@@ -10,8 +10,9 @@ import SwiftUIFontIcon
 
 struct PopupView: View {
     @State private var showSettings = false
-
+    
     var body: some View {
+        ZStack {
             VStack {
                 HStack {
                     Button(action: {}) {
@@ -24,18 +25,25 @@ struct PopupView: View {
                         .foregroundColor(Color("Pomodoro Primary"))
                         .frame(width: 40, height: 40)
                 }
-                ZStack {
-                    AnimationView()
-                    if showSettings {
-                        SettingsView()
-                    }
-                }
+                AnimationView()
             }
-        
+            if showSettings {
+                Rectangle()
+                    .fill(.black.opacity(0.15))
+                    .onTapGesture {
+                        closeSettings()
+                    }
+                SettingsView()
+            }
+        }
     }
     
     func openSettings() {
         showSettings = true
+    }
+    
+    func closeSettings() {
+        showSettings = false
     }
 }
 
