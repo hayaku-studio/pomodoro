fe4052de252e0c19cc8604b4b364c1f44c4e5263
David Speers <speerscd@gmail.com>
2023-06-23 16:48:59 +0100

    remove mergePolicy
    cause I don't know enough about it yet and would rather get an error if it happens while developing, which is the default policy

--- Pomodoro/PersistenceController.swift
+++ Pomodoro/PersistenceController.swift
@@ -11,36 +11,39 @@ import CoreData
 struct PersistenceController {
     // A singleton for our entire app to use
     static let shared = PersistenceController()
-
+    
     // Storage for Core Data
     let container: NSPersistentContainer
-
+    
     // TODO: doesn't seem to work
     // A test configuration for SwiftUI previews
     static var preview: PersistenceController = {
         let controller = PersistenceController(inMemory: true)
-
+        
         // Create 10 example calendar entries.
         for _ in 0..<10 {
             let entry = CalendarEntry(context: controller.container.viewContext)
             entry.date = Date.now
             entry.workTimeMinutes = 120
         }
-
+        
         return controller
     }()
-
+    
     // An initializer to load Core Data, optionally able
     // to use an in-memory store.
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "DataModel")
-
+        
         if inMemory {
             container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
         }
-
+        
+        // TODO: look into this mergePolicy and if it's necessary
+        // https://www.hackingwithswift.com/read/38/6/how-to-make-a-core-data-attribute-unique-using-constraints
+        // container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        
         container.loadPersistentStores { description, error in
-            self.container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
             
             if let error = error {
                 fatalError("Unresolved Init Error: \(error.localizedDescription)")
@@ -50,7 +53,7 @@ struct PersistenceController {
     
     func save() {
         let context = container.viewContext
-
+        
         if context.hasChanges {
             do {
                 try context.save()
