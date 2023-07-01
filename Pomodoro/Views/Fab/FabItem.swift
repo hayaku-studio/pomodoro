849ebb01ef0618117e86f428d02a6643319176ab
David Speers <speerscd@gmail.com>
2023-07-01 12:15:58 +0100

    scale FabItem onHover

--- Pomodoro/Views/Fab/FabItem.swift
+++ Pomodoro/Views/Fab/FabItem.swift
@@ -12,34 +12,52 @@ struct FabItem: View {
     var buttonText: String
     
     @State private var isHovered = false
-    let imageWidth: CGFloat = 16
+    
+    var fontSize: CGFloat {
+        return isHovered ? 15 : 14
+    }
+    var iconSize: CGFloat {
+        return isHovered ? 17 : 16
+    }
     
     var body: some View {
-        HStack {
-            Text(buttonText)
-                .font(.system(size: 14, design: .default))
-                .foregroundColor(Color("Button Active"))
-                .offset(y: -1)
-                .fixedSize(horizontal: true, vertical: true)
-            Spacer()
-            Image(systemName: imageName)
-                .resizable()
-                .scaledToFit()
-                .foregroundColor(Color("Button Active"))
-                .frame(width: imageWidth, height: imageWidth)
+        ZStack {
+            Rectangle()
+                .fill(Color(isHovered ? "FAB Background Hovered" : "FAB Background"))
+                .setFabItemFrame()
+                .cornerRadius(8)
+                .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 1)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color(hex: 0xF4F4F4), lineWidth: 1)
+                )
+                .scaleEffect(isHovered ? 1.1 : 1)
+            HStack {
+                Text(buttonText)
+                    .font(.system(size: fontSize, design: .default))
+                    .foregroundColor(Color("Button Active"))
+                    .offset(y: -1)
+                    .fixedSize(horizontal: true, vertical: true)
+                Spacer()
+                Image(systemName: imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(Color("Button Active"))
+                    .frame(width: iconSize, height: iconSize)
+            }
+            .padding(.horizontal, 8)
+            .setFabItemFrame()
         }
-        .padding(.horizontal, 8)
-        .frame(width: 100, height: 30)
-        .background(Color(isHovered ? "FAB Background Hovered" : "FAB Background"))
-        .cornerRadius(8)
-        .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 1)
-        .overlay(
-            RoundedRectangle(cornerRadius: 8)
-                .stroke(Color(hex: 0xF4F4F4), lineWidth: 1)
-        )
+        .offset(x: 4)
         .onHover { isHovered in
             self.isHovered = isHovered
         }
+    }
+}
+
+extension View {
+    func setFabItemFrame() -> some View {
+        self.frame(width: 100, height: 30)
     }
 }
 
