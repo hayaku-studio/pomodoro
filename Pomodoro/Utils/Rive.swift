7757799980b2caf9b290a51b13c1c709dca5219c
David Speers <speerscd@gmail.com>
2023-11-05 14:52:50 +0800

    fix `jump at 25` bug
    both 5 and 20 are like 0 in that they behave differently depending on which side of 0 they're on

--- Pomodoro/Utils/Rive.swift
+++ Pomodoro/Utils/Rive.swift
@@ -12,9 +12,13 @@ func updateTimeInput(riveViewModel: RiveViewModel, minutes: Float) {
     riveViewModel.setInput("timeMinutes", value: minutes)
     let loopedMinutes = minutes.truncatingRemainder(dividingBy: 25)
     let magnificationFactor: Float = 15.0
-    let absoluteDifference = abs(loopedMinutes < 12.5 ? 0 - loopedMinutes : 25 - loopedMinutes)
-    riveViewModel.setInput("0 Magnified", value: (100 - absoluteDifference * magnificationFactor))
-    for timeToMagnify in [5, 10, 15, 20] {
+    let absoluteDifference0 = abs(loopedMinutes < 12.5 ? 0 - loopedMinutes : 25 - loopedMinutes)
+    riveViewModel.setInput("0 Magnified", value: (100 - absoluteDifference0 * magnificationFactor))
+    for timeToMagnify: Float in [5, 20] {
+        let absoluteDifference = abs(loopedMinutes < abs(timeToMagnify - 12.5) ? timeToMagnify - loopedMinutes : timeToMagnify - loopedMinutes)
+        riveViewModel.setInput("\(Int(timeToMagnify)) Magnified", value: (100 - absoluteDifference * magnificationFactor))
+    }
+    for timeToMagnify in [10, 15] {
         riveViewModel.setInput("\(timeToMagnify) Magnified", value: 100 - abs(Float(timeToMagnify) - loopedMinutes) * magnificationFactor)
     }
 }
