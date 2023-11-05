7ef9896f97606370e9eccda7be09f8c522abf3a4
David Speers <speerscd@gmail.com>
2023-11-05 16:05:31 +0800

    countdown last 30s

--- Pomodoro/Views/PomodoroIconView.swift
+++ Pomodoro/Views/PomodoroIconView.swift
@@ -12,7 +12,8 @@ struct PomodoroIconView: View {
     @EnvironmentObject private var modelData: ModelData
     
     var body: some View {
-        let minutes = Int(ceilf(Float(modelData.timeSeconds)/60))
+        let isTimeLessThan30s = modelData.timeSeconds <= 30 && modelData.timeSeconds > 0
+        let time = isTimeLessThan30s ? modelData.timeSeconds : Int(round(Float(modelData.timeSeconds)/60))
         let isFocusFlowType = modelData.flowType == FlowType.focus
         return HStack(alignment: .center, spacing: isFocusFlowType ? 2 : 0) {
             ZStack {
@@ -23,12 +24,12 @@ struct PomodoroIconView: View {
                     Image("Coffee Cup Icon")
                         .offset(x: 2, y: 2)
                 }
-                Text("\(String(format: "%02d", minutes))")
+                Text("\(String(format: "%02d", time))")
                     .font(.footnote)
                     .fontWeight(.semibold)
                     .monospacedDigit()
                     .offset(y: 2)
-                    .foregroundColor(Color("Dark Mode Contrast"))
+                    .foregroundColor(Color("Dark Mode Contrast\(isTimeLessThan30s ? " Lighter" : "")"))
             }
         }
     }
