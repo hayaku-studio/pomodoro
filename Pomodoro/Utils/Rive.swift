c6f262849d3e5945258351e73d42fcff9e9d591d
David Speers <speerscd@gmail.com>
2023-11-18 15:50:24 +0800

    fix `jump around 25` bug properly and add tests

--- Pomodoro/Utils/Rive.swift
+++ Pomodoro/Utils/Rive.swift
@@ -8,6 +8,8 @@
 import Foundation
 import RiveRuntime
 
+// TODO: should this file be a class?
+
 func updateTimeInput(riveViewModel: RiveViewModel, minutes: Float) {
     riveViewModel.setInput("timeMinutes", value: minutes)
     for timeToMagnify in TimeToMagnify.allCases {
@@ -17,14 +19,19 @@ func updateTimeInput(riveViewModel: RiveViewModel, minutes: Float) {
 
 func getMagnificationFactor(timeToMagnify: TimeToMagnify, timeMinutes: Float) -> Float {
     let loopedMinutes = timeMinutes.truncatingRemainder(dividingBy: 25)
-    let magnificationFactor: Float = 10.0
+    // print(loopedMinutes) == 0
+    let magnificationFactor: Float = 15.0
     switch timeToMagnify {
     case .time0:
         let absoluteDifference0 = abs(loopedMinutes < 12.5 ? 0 - loopedMinutes : 25 - loopedMinutes)
         return 100 - absoluteDifference0 * magnificationFactor
     case .time10, .time15:
         return 100 - abs(timeToMagnify.numberValue - loopedMinutes) * magnificationFactor
-    case .time5, .time20:
-        let absoluteDifference = abs(loopedMinutes < abs(timeToMagnify.numberValue - 12.5) ? timeToMagnify.numberValue - loopedMinutes : timeToMagnify.numberValue - loopedMinutes)
+    case .time20:
+        let absoluteDifference = abs(loopedMinutes < abs(timeToMagnify.numberValue - 12.5) ? 25 - timeToMagnify.numberValue + loopedMinutes : timeToMagnify.numberValue - loopedMinutes)
         return 100 - absoluteDifference * magnificationFactor
-    }}
+    case .time5:
+        let absoluteDifference = loopedMinutes >= 20 ? 25 - abs(timeToMagnify.numberValue - loopedMinutes) : abs(timeToMagnify.numberValue - loopedMinutes)
+        return 100 - absoluteDifference * magnificationFactor
+    }
+}
--- Whimsical Pomodoro TimerTests/UtilsRiveTests.swift
+++ Whimsical Pomodoro TimerTests/UtilsRiveTests.swift
@@ -72,4 +72,44 @@ final class UtilsRiveTests: XCTestCase {
             XCTAssertEqual(Int(getMagnificationFactor(timeToMagnify: TimeToMagnify.time15, timeMinutes: Float(input))), expectedOutput)
         }
     }
+    
+    func testTime20() throws {
+        let timeMinutesToExpectedMagnificationFactor: [Int: Int] = [
+            10: 100-10*magnificationFactor,
+            14: 100-6*magnificationFactor,
+            16: 100-4*magnificationFactor,
+            18: 100-2*magnificationFactor,
+            20: 100,
+            21: 100-1*magnificationFactor,
+            23: 100-3*magnificationFactor,
+            26: 100-6*magnificationFactor, // this test fails
+            25: 100-5*magnificationFactor, // this test fails
+            45: 100
+        ]
+
+        for (input, expectedOutput) in timeMinutesToExpectedMagnificationFactor {
+            let actualOutput = Int(getMagnificationFactor(timeToMagnify: TimeToMagnify.time20, timeMinutes: Float(input)))
+            XCTAssertEqual(actualOutput, expectedOutput, "\nWith Input: \(input) \nActual value: \(actualOutput) \nExpected value: \(expectedOutput)\n")
+        }
+    }
+    
+    func testTime5() throws {
+        let timeMinutesToExpectedMagnificationFactor: [Int: Int] = [
+            20: 100-10*magnificationFactor,
+            24: 100-6*magnificationFactor,
+            26: 100-4*magnificationFactor,
+            28: 100-2*magnificationFactor,
+            30: 100,
+            31: 100-1*magnificationFactor,
+            33: 100-3*magnificationFactor,
+            36: 100-6*magnificationFactor, // this test fails
+            35: 100-5*magnificationFactor, // this test fails
+            55: 100
+        ]
+
+        for (input, expectedOutput) in timeMinutesToExpectedMagnificationFactor {
+            let actualOutput = Int(getMagnificationFactor(timeToMagnify: TimeToMagnify.time5, timeMinutes: Float(input)))
+            XCTAssertEqual(actualOutput, expectedOutput, "\nWith Input: \(input) \nActual value: \(actualOutput) \nExpected value: \(expectedOutput)\n")
+        }
+    }
 }
